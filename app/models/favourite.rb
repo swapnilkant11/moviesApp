438d6489a6770cd9b17416f16702e804b97cca07
Swapnil Kant <swapnilkant@Swapnils-MacBook-Pro.local>
2022-06-22 09:52:31 +0530

    favourite controller, validation added

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -2,4 +2,10 @@ class Favourite < ApplicationRecord
     include Filterable
     belongs_to :user
     belongs_to :movie
+    validates :title, presence: true
+    validates :imdbID, presence: true
+    validates :year, presence: true
+    validates :remainder_date, presence: true
+    validates :user_id, presence: true
+    validates :movie_id, presence: true
 end
